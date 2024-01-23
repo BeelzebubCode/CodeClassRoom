f0c75a86dc610126fc22bd6cb40bf2610cb4be6f
Beezebub <pongrock1234@gmail.com>
2024-01-24 01:57:23 +0700

    Task class room 24/1/2567

---
+++ src/Circle.java
@@ -0,0 +1,23 @@
+public class Circle extends Shape{
+    private int radius;
+
+    public Circle(String colour, int radius){
+        super(colour);
+        this.radius = radius;
+    }
+
+    @Override
+    public int getRadius(){
+        return radius;
+    }
+
+    @Override
+    public double getArea(){
+        return Math.PI*radius*radius;
+    }
+
+    @Override
+    public String toString(){
+        return "Circle ("+radius+"): "+super.toString();
+    }
+}
---
+++ src/Shape.java
@@ -0,0 +1,31 @@
+public class Shape {
+    private String colour;
+
+    public Shape(String colour){
+        this.colour = colour;
+    }
+
+    public String toString(){
+        return "This shape is "+colour;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public double getArea(){
+        return 0;
+    }
+
+    public int getWidth(){
+        return 0;
+    }
+
+    public int getRadius(){
+        return 0;
+    }
+}
